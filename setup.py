473b1a6ec6097e52fa7830b07f592627bc4deda8
Michael Rans <rans@email.com>
2017-11-21 14:17:25 +0100

    Fix import

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ def get_readme():
     return readme_file.read()
 
 
-requirements = ['hdx-data-freshness>=0.99.5']
+requirements = ['hdx-data-freshness>=0.99.6']
 
 classifiers = [
     "Development Status :: 5 - Production/Stable",
@@ -63,7 +63,7 @@ setup(
     description='HDX Data Freshness Emailer',
     license='MIT',
     url='https://github.com/OCHA-DAP/hdx-data-freshness',
-    version='0.27',
+    version='0.28',
     author='Michael Rans',
     author_email='rans@email.com',
     keywords=['HDX', 'fresh', 'freshness', 'data freshness'],
