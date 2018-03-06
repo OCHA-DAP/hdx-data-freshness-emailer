7d38bf65eec3e8d0d3cf4bd65780d752d04cc4e6
Mike <rans@email.com>
2018-03-06 07:23:24 +0100

    version

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ setup(
     description='HDX Data Freshness Emailer',
     license='MIT',
     url='https://github.com/OCHA-DAP/hdx-data-freshness',
-    version='0.48',
+    version='0.49',
     author='Michael Rans',
     author_email='rans@email.com',
     keywords=['HDX', 'fresh', 'freshness', 'data freshness'],
