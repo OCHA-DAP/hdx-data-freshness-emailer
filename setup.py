79b8645d145d12683585c06ca2d3a94137250ee1
Mike <rans@email.com>
2020-01-29 09:51:24 +0100

    Update to latest freshness

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ def get_readme():
 
 
 requirements = ['pygsheets',
-                'hdx-data-freshness>=1.4.5']
+                'hdx-data-freshness>=1.4.6']
 
 classifiers = [
     "Development Status :: 5 - Production/Stable",
